c6c46859e31f63e0ef474ec1fd74f5fecf661c42
Ubuntu <ubuntu@ip-172-31-13-233.ap-south-1.compute.internal>
2025-12-17 11:34:58 +0000

    sixth commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express'); 
 var app = express();//Respond with "hello world" for requests that hit our root "/" 
 app.get('/', function (req, res) { 
- res.send('this is 5 build '); 
+ res.send('this is 6 build '); 
 });//listen to port 3000 by default 
 app.listen(process.env.PORT || 3000); 
 module.exports = app; 
